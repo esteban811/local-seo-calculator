4eb0a75afbe241ddb2bfa893b249ab8fbe8e6d8a
esteban811 <esteban@makariosmarketing.com>
2026-01-28 16:54:26 -0500

    Fix TypeScript errors

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { CalculatorInput, ServiceType, PopulationTier, GMBPosition } from '../types/calculator';
 
 interface CalculatorProps {
